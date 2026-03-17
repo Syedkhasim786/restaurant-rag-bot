2a708820c4b6e96f0750168bbfce8c24f0ae352b
Syedkhasim786 <syedkhasim2001@gmail.com>
2026-03-17 21:49:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ Answer clearly.
 """
     try:
         response = client.models.generate_content(
-            model="gemini-2.0-flash", 
+            model="gemini-1.5-flash", 
             contents=prompt
         )
         return response.text
